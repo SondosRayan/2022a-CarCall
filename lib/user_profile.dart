d2f2487c9a35df22ff3e67b07a78e9c605b6af1c
SondosRayan <93395502+SondosRayan@users.noreply.github.com>
2021-12-28 18:43:34 +0200

    authentication with google done #3

--- lib/user_profile.dart
+++ lib/user_profile.dart
@@ -1,22 +1,24 @@
-// import 'package:car_call/auth_repository.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-//
-// class UserProfile{
-//   // late User? user=AuthRepository.instance().user;
-//   late String name;
-//   late String email;
-//   late String password;
-//   late String gender;
-//   late String birthDate; // ?? check
-//   late String phoneNumber;
-//   late String carPlate;
-//   // to add blocked list (list of uid)
-//
-//
-//   Future<String> getBirthDate(User user) async {
-//     await AuthRepository.instance().firebaseFirestore.collection('Users')
-//         .doc(user!.uid)
-//         .update({data});
-//   }
-//
-// }
+import 'package:car_call/auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserProfile{
+  // late User? user=AuthRepository.instance().user;
+  final String uid;
+  late String name;
+  late String email;
+  late String password;
+  late String gender;
+  late String birthDate; // ?? check
+  late String phoneNumber;
+  late String carPlate;
+  // to add blocked list (list of uid)
+
+  UserProfile({required this.uid});
+
+  // Future<String> getBirthDate(User user) async {
+  //   await AuthRepository.instance().firebaseFirestore.collection('Users')
+  //       .doc(user!.uid)
+  //       .update({data});
+  // }
+
+}

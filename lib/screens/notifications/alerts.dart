c9c5405eb31f4707db0b6cb37e2d3d187abf8e3e
SondosRayan <93395502+SondosRayan@users.noreply.github.com>
2022-01-18 19:36:37 +0200

    Update alerts.dart

--- lib/screens/notifications/alerts.dart
+++ lib/screens/notifications/alerts.dart
@@ -1,12 +1,14 @@
+
 import 'package:car_call/auth_repository.dart';
-import 'package:car_call/screens/chat_room.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../dataBase.dart';
 import '../../globals.dart';
+import '../navigation_bar.dart';
 
+Set<String> UnreadNotifications = Set<String>();
 
 
 class NotificationsScreen extends StatefulWidget {
@@ -16,25 +18,22 @@ class NotificationsScreen extends StatefulWidget {
   _NotificationsScreenState createState() => _NotificationsScreenState();
 }
 
-class _NotificationsScreenState extends State<NotificationsScreen> {
+class _NotificationsScreenState extends State<NotificationsScreen>{
   late AuthRepository auth ;
   final db = getDB();
+
+
 
   @override
   Widget build(BuildContext context){
     auth = Provider.of<AuthRepository>(context);
+
     return Scaffold(
       backgroundColor: blue1,
           appBar: AppBar(
             backgroundColor: green11,
             automaticallyImplyLeading: false,
-            /*bottom: const TabBar(
-              indicatorColor: Colors.white,
-              tabs: [
-                Tab(icon: Text("Alerts")),
-                Tab(icon: Text("Help Offers")),
-              ],
-            ),*/
+
             title: const Text('Notifications'),
             centerTitle: true,
 
@@ -62,7 +61,7 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
                 children: [
                   StreamBuilder<QuerySnapshot>(
                     stream: db.collection('Users').doc(auth.user!.uid)
-                        .collection('Alerts').snapshots(),
+                        .collection('Alerts').orderBy('timestamp', descending: true).snapshots(),
                     builder: (context, snapshot) {
                       if (!snapshot.hasData) {
                         return const Center(
@@ -71,31 +70,67 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
                       } else {
                         return ListView(
                           children: snapshot.data!.docs.map((doc){
-                            return Card(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius:BorderRadius.all(Radius.circular(10.0))),
-                              child: ListTile(
-                                  trailing: IconButton(icon: Icon(Icons.block), color: green11,
-                                    onPressed: (){},
-                                  ),
-                                  title:
-                                  RichText(
-                                    text: TextSpan(
+                            Timestamp notification_time = doc.get('timestamp');
+                            DateTime notification_date = notification_time.toDate();
+                            Duration diff = DateTime.now().difference(notification_date);
+                            String stringTimeAgo = getDifference(diff);
+                            if(doc.get('read') == false){
+                              UnreadNotifications.add('A'+doc.id);
+                            }
+
+                            return GestureDetector(
+                              onTap: () => {
+                                setState(() {
+                                  doc.reference.update({'read': true});
+                                  UnreadNotifications.remove('A'+doc.id);
+                                })
+                              },
+                              child: Card(
+                                color : (doc.get('read') == true? Colors.white : blue2),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:BorderRadius.all(Radius.circular(10.0))),
+                                  child: Column(
+                                    children: [
+                                      ListTile(
+                                          trailing: IconButton(icon: Icon(Icons.block), color: green11,
+                                            onPressed: (){
+                                              _showBlockDialog(doc.get('sender'), context);
+                                            },
+                                          ),
+                                          title:
+                                          RichText(
+                                            text: TextSpan(
+                                                children: [
+                                                  TextSpan(text: "Hi ${auth.firstName}, ${doc.get('sender_name')}"
+                                                    +" sent you an alert about ", style: TextStyle(color: green11),),
+                                                  TextSpan(text: doc.get('type'), style: TextStyle(color: green11, fontWeight: FontWeight.bold),),
+                                                  TextSpan(text: ".", style: TextStyle(color: green11),),
+                                                  TextSpan(text: (doc.get('type') == 'Car Crash')?
+                                                      "You can contact ${doc.get('sender_name')} on this phone number "
+                                                      +doc.get('phoneNumber')+"."
+                                                      : null, style: TextStyle(color: green11) ),
+                                                ]
+                                            ),
+                                          )
+                                      ),
+                                      Column(
                                         children: [
-                                          TextSpan(text: "Hi ${auth.firstName}, ${doc.get('sender_name')}"
-                                            +" sent you an alert about ", style: TextStyle(color: green11),),
-                                          TextSpan(text: doc.get('type'), style: TextStyle(color: green11, fontWeight: FontWeight.bold),),
-                                          TextSpan(text: ".", style: TextStyle(color: green11),),
-                                          TextSpan(text: (doc.get('type') == 'Car Crash')?
-                                              "You can contact ${doc.get('sender_name')} on this phone number "
-                                              +doc.get('phoneNumber')+"."
-                                              : null, style: TextStyle(color: green11) ),
-                                        ]
-                                    ),
-                                  )
-                              ),
+                                          Column(
+                                            children: [
+                                              Container(
+                                                alignment: Alignment.bottomRight,
+                                                child: getText( stringTimeAgo+ "   ",Colors.blueGrey, 11, false),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
                             );
                           }).toList(),
+
                         );
                       }
                     },
@@ -111,37 +146,47 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
                       } else {
                         return ListView(
                           children: snapshot.data!.docs.map((doc){
-                            return Card(
-                              shape: RoundedRectangleBorder(borderRadius:BorderRadius.all(Radius.circular(10.0))),
-                              child: ListTile(
-                                trailing: IconButton(
-                                  icon: Icon(Icons.chat),color: green11,
-                                  onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(builder: (context) =>
-                                          ChatRoomScreen(
-                                            userId: auth.user!.uid,
-                                            userName: auth.fullName,
-                                            peerId: doc.get('sender'),
-                                            peerName: doc.get('sender_name'),
-                                            peerAvatar: "",) //TODO
-                                      ),
-                                    );
-                                  },
-                                ),
-                                  title:
-                                  RichText(
-                                    text: TextSpan(
-                                        children: [
-                                          TextSpan(text: "Hi ${auth.firstName}, you received a help offer from "
-                                              +"${doc.get('sender_name')}, regarding your ",
-                                            style: TextStyle(color: green11),),
-                                          TextSpan(text: doc.get('type'), style: TextStyle(color: green11, fontWeight: FontWeight.bold),),
-                                          TextSpan(text: " request.", style: TextStyle(color: green11),),
-                                        ]
+                            Timestamp notification_time = doc.get('timestamp');
+                            DateTime notification_date = notification_time.toDate();
+                            Duration diff = DateTime.now().difference(notification_date);
+                            String stringTimeAgo = getDifference(diff);
+                            if(doc.get('read') == false){
+                              UnreadNotifications.add('O'+doc.id);
+                            }
+
+                            return GestureDetector(
+                              onTap: () => {
+                                setState(() {
+                                  doc.reference.update({'read': true});
+                                  UnreadNotifications.remove('O'+doc.id);
+                                })
+                              },
+                              child: Card(
+                                color : (doc.get('read') == true? Colors.white : blue2),
+                                shape: RoundedRectangleBorder(borderRadius:BorderRadius.all(Radius.circular(10.0))),
+                                child: Column(
+                                  children: [
+                                    ListTile(
+                                      trailing: IconButton(icon: Icon(Icons.chat),color: green11, onPressed: () {},),
+                                        title:
+                                        RichText(
+                                          text: TextSpan(
+                                              children: [
+                                                TextSpan(text: "Hi ${auth.firstName}, you received a help offer from "
+                                                    +"${doc.get('sender_name')}, regarding your ",
+                                                  style: TextStyle(color: green11),),
+                                                TextSpan(text: doc.get('type'), style: TextStyle(color: green11, fontWeight: FontWeight.bold),),
+                                                TextSpan(text: " request.", style: TextStyle(color: green11),),
+                                              ]
+                                          ),
+                                        )
                                     ),
-                                  )
+                                    Container(
+                                      alignment: Alignment.bottomRight,
+                                      child: getText( stringTimeAgo + "   ",Colors.blueGrey, 11, false),
+                                    ),
+                                  ],
+                                ),
                               ),
                             );
                           }).toList(),
@@ -154,6 +199,80 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
         ),
       ),        );
   }
+
+  Future<void> _showBlockDialog(String to_Block, context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: SingleChildScrollView(
+            child: ListBody( children: <Widget>[
+              Text('Are you sure you want to block this user?')
+            ],
+            ),
+          ),
+          actions: <Widget>[Row(
+            children: [
+              TextButton(
+                child: Text('Yes', style: TextStyle(color: green11,),),
+                onPressed: () async{
+                  auth.blockUser(to_Block, 'annoying alerts');
+                  Navigator.of(context).pop();
+                },
+              ),
+              const Spacer(),
+              TextButton(
+                child: Text('No', style: TextStyle(color: green11),),
+                onPressed: () {Navigator.of(context).pop();},
+              ),
+            ],
+          ),
+          ],
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+        );
+      },
+    );
+  }
+
+  String getDifference(Duration diff){
+    int DaysAgo = diff.inDays;
+    int HoursAgo = diff.inHours;
+    int MinutesAgo = diff.inMinutes;
+    int secondsAgo = diff.inSeconds;
+    String stringTimeAgo = "";
+    if(DaysAgo > 0){
+      if(HoursAgo == 1){
+        stringTimeAgo = DaysAgo.toString()+" Day Ago";
+      }else{
+        stringTimeAgo = DaysAgo.toString()+" Days Ago";
+      }
+    }
+    else if(HoursAgo > 0){
+      if(HoursAgo == 1){
+        stringTimeAgo = HoursAgo.toString()+" Hour Ago";
+      }else{
+        stringTimeAgo = HoursAgo.toString()+" Hours Ago";
+      }
+    }
+    else if(MinutesAgo >0){
+      if(HoursAgo == 1){
+        stringTimeAgo = MinutesAgo.toString()+" Minute Ago";
+      }else{
+        stringTimeAgo = MinutesAgo.toString()+" Minutes Ago";
+      }
+    }
+    else if(secondsAgo >=3){
+      stringTimeAgo = secondsAgo.toString()+" Seconds Ago";
+    }
+    else{
+      stringTimeAgo = "just now";
+    }
+    return stringTimeAgo;
+  }
+
+
 
 
 
